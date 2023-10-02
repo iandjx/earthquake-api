0a8eeaa7423e70e768e017d843eee11efcc55bee
Ian Christopher B. de Jesus <3683356+iandjx@users.noreply.github.com>
2023-10-02 10:25:53 +0800

    add getAveMagPerMonth function

--- src/services/earthquakeService.ts
+++ src/services/earthquakeService.ts
@@ -2,6 +2,7 @@ import axios from 'axios'
 import { EARTHQUAKE_URL } from '../constants'
 import { EarthquakeModel } from '../models/earthquakeModel'
 import { Earthquake, RawEarthquake } from '../types'
+import dayjs from 'dayjs'
 
 export enum Comparators {
   gt = '>',
@@ -38,6 +39,7 @@ export interface EarthquakeService {
   findEarthquakeData: (
     options?: QueryOptions,
   ) => Promise<Earthquake[] | undefined>
+  getAveMagPerMonth: (year: number) => Promise<number[]>
 }
 
 const createEarthquakeService = (
@@ -47,13 +49,15 @@ const createEarthquakeService = (
     fetchEarthquakeData: async () => {
       try {
         const res = await axios.get(EARTHQUAKE_URL)
-        const formattedData = res.data.map((earthquake: RawEarthquake) => {
-          return {
-            ...earthquake.properties,
-            coordinates: earthquake.geometry.coordinates,
-            id: earthquake.id,
-          }
-        }) as Earthquake[]
+        const formattedData = res.data.features.map(
+          (earthquake: RawEarthquake) => {
+            return {
+              ...earthquake.properties,
+              coordinates: earthquake.geometry.coordinates,
+              id: earthquake.id,
+            }
+          },
+        ) as Earthquake[]
         await earthquakeModel.saveEarthquakeData(formattedData)
       } catch (error) {
         return Error('Error fetching earthquake data')
@@ -62,7 +66,44 @@ const createEarthquakeService = (
     findEarthquakeData: (options?: QueryOptions) => {
       return earthquakeModel.queryEarthquakeData(options)
     },
+    getAveMagPerMonth: async (year: number) => {
+      const earthquakeData = await earthquakeModel.queryEarthquakeData()
+      if (!earthquakeData) {
+        return []
+      }
+      const averages = calculateMonthlyAverages(earthquakeData, year)
+      return averages
+    },
   }
 }
 
 export default createEarthquakeService
+
+export const calculateMonthlyAverages = (
+  earthquakes: Earthquake[],
+  targetYear: number,
+): number[] => {
+  const monthlyAverages: number[] = new Array(12).fill(0)
+
+  const earthquakeCountsPerMonth: number[] = new Array(12).fill(0)
+
+  earthquakes.forEach((earthquake) => {
+    const earthquakeYear = dayjs(earthquake.time).year()
+
+    if (earthquakeYear === targetYear) {
+      const earthquakeMonth = dayjs(earthquake.time).month()
+      const earthquakeMag = earthquake.mag
+
+      monthlyAverages[earthquakeMonth] += earthquakeMag
+      earthquakeCountsPerMonth[earthquakeMonth] += 1
+    }
+  })
+
+  for (let i = 0; i < 12; i++) {
+    if (earthquakeCountsPerMonth[i] !== 0) {
+      monthlyAverages[i] /= earthquakeCountsPerMonth[i]
+    }
+  }
+
+  return monthlyAverages
+}
